685d29bf85077262bde2067de30bcb60c6c346e0
Sergio Guerra <sergioguerra6188@gmail.com>
2023-03-04 13:16:19 -0500

    Add a loop that chooses word between 5-12 letters
    Loop condition will be 6 and 13 because the newline character counts.
    There is also a check if the word chosen is nil to prevent a runtime
    error. Further testing might be needed.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,8 +1,11 @@
 def choose_random_word
     dictionary = File.open("google-10000-english-no-swears.txt", "r")
-    word_number = rand(9894) + 1
-    word = ''
-
-    word_number.times { word = dictionary.gets }
+    word = "A"
+    
+    while (word.length < 6 || word.length > 13)
+        word_number = rand(9894) + 1
+        word_number.times { word = dictionary.gets }
+        word = "A" if word == nil
+    end
     return word
 end
