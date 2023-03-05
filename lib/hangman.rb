3f0874acea98bbeb23229568927c72ca5ba84f49
Sergio Guerra <sergioguerra6188@gmail.com>
2023-03-04 22:36:41 -0500

    Rework generating random word to use exc-handling
    Exception handling is included into the method just in case the ruby
    program is unable to find a word.
    
    The whole file is also read into the variable as the file is not large
    enough to cause issues.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,15 +1,21 @@
 require 'pry-byebug'
 
 def choose_random_word
-    dictionary = File.open("google-10000-english-no-swears.txt", "r")
-    word = "A"
-    
-    while (word.length < 6 || word.length > 13)
-        word_number = rand(9894) + 1
-        word_number.times { word = dictionary.gets }
-        word = "A" if word == nil
+    begin
+        dictionary = File.open("google-10000-english-no-swears.txt", "r")
+        word = "A"
+
+        while (word.length < 6 || word.length > 13)
+            word_number = rand(9894) + 1
+            word = dictionary.readlines[word_number]
+            word = "A" if word == nil
+        end
+        return word
+        dictionary.close
+    rescue
+        "Could not generate word, try again later :("
+        exit
     end
-    return word
 end
 
 class Game
